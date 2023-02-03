d61ba5f97c06e566445571b21e12f67d940745a7
Vivek Khandelwal <vivekkhandelwal1424@gmail.com>
2023-02-03 11:39:13 +0530

    add squeeze dims op

--- lib/Dialect/Torch/Transforms/LowerToBackendContract.cpp
+++ lib/Dialect/Torch/Transforms/LowerToBackendContract.cpp
@@ -445,6 +445,7 @@ static void markDecomposedOpsAsIllegal(MLIRContext *context,
   target.addIllegalOp<AtenNewEmptyStridedOp>();
   target.addIllegalOp<AtenBucketizeTensorOp>();
   target.addIllegalOp<AtenMovedimIntOp>();
+  target.addIllegalOp<AtenSqueezeDimsOp>();
   for (std::string opName : backendLegalOps) {
     target.addLegalOp(OperationName(opName, context));
   }
